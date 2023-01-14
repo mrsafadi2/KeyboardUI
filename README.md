76dc4f03717a622d864e2afd2afda2ba08bb2df8
mrsafadi2 <mrsafadi2@gmail.com>
2023-01-14 23:46:55 +0200

    initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # KeyboardManager
 
-A description of this package.
+KeyboardManager
+
+This project is only for SwiftUI
+
+When View uses ignoresSafeArea or edgesIgnoringSafeArea(.bottom)
+
+this framework can be used because TextField and TextEditor cannot make the keyboard bounce
+
+Encapsulated UITextView, UITextField can also be used
+Supported Platforms
+
+    iOS 13.0
+    macOS 10.15
+    tvOS 13.0
+    watchOS 6.0
+
+Usage
+
+    @State private var text: String = ""
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            KeyboardHost {
+                TextField("please enter text", text: $text)
+                    .frame(width: 200, height: 40, alignment: .center)
+                    .padding(.bottom, 20)
+            }
+        }
+        .ignoresSafeArea()
+    }
+
+Installation
+
+You can add KeyboardHost to an Xcode project by adding it as a package dependency.
+
+    From the File menu, select Swift Packages › Add Package Dependency…
+    Enter https://github.com/mrsafadi2/KeyboardManager into the package repository URL text field
+    Link KeyboardManager to your application target
+
+GitHub
+
+ 
--- Sources/KeyboardManager/KeyboardManager.swift
+++ Sources/KeyboardManager/KeyboardManager.swift
@@ -1,6 +1,50 @@
-public struct KeyboardManager {
-    public private(set) var text = "Hello, World!"
+//
+//  KeyboardManager.swift
+//  CoreDataSwiftUI
+//
+//  Created by i mac on 12/01/2023.
+//
 
-    public init() {
+import SwiftUI
+
+public struct KeyboardManager<Content>: View where Content: View {
+    
+    /// - Parameter content: some View
+    public init (@ViewBuilder content: () -> Content) {
+        view = content()
+    }
+    
+    public var body: some View {
+        VStack {
+            view
+            Spacer()
+                .frame(height: keyboardHeight)
+                .animation(.default)
+                .foregroundColor(.clear)
+        }.onReceive(showPublisher.merge(with: hidePublisher)) { (height) in
+            self.keyboardHeight = height
+        }
+    }
+    
+    let view: Content
+    
+    @State private var keyboardHeight: CGFloat = 0
+    
+    private let showPublisher = NotificationCenter.Publisher.init(
+        center: .default,
+        name: UIResponder.keyboardWillShowNotification
+    ).map { (notification) -> CGFloat in
+        if let rect = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            return rect.size.height
+        } else {
+            return 0
+        }
+    }
+    
+    private let hidePublisher = NotificationCenter.Publisher.init(
+        center: .default,
+        name: UIResponder.keyboardWillHideNotification
+    ).map { _ -> CGFloat in
+        return 0
     }
 }
